86ed03ccc95e8bcb4d512077600d30b0134146a5
Kuber Patel <101393723+kuberpatel@users.noreply.github.com>
2023-11-10 14:38:49 +0530

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -1,22 +1,36 @@
+// Require the Mongoose module
 const mongoose = require('mongoose');
-const employee = new mongoose.Schema({
-    firstname:{
-        type : String,
-        required : true
-    },
-    lastname:{
-        type : String,
-        required : true
-    },
-    email : {
-        type : String,
-        required : true,
-        unique:true
-    },
-    password : {
-        type : String,
-        required : true
-    }
-},{timestamps : true});
-const Employee = mongoose.model('Employee' ,employee);
+
+// Define a schema for an employee document
+const employeeSchema = new mongoose.Schema({
+  // First name of the employee
+  firstName: {
+    type: String,
+    required: true,
+  },
+  // Last name of the employee
+  lastName: {
+    type: String,
+    required: true,
+  },
+  // Email address of the employee
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // Password of the employee
+  password: {
+    type: String,
+    required: true,
+  },
+}, {
+  // Add timestamps to the document
+  timestamps: true,
+});
+
+// Create a Mongoose model for the employee schema
+const Employee = mongoose.model('Employee', employeeSchema);
+
+// Export the Employee model so that it can be used in other modules
 module.exports = Employee;
